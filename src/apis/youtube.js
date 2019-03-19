28337770c1859db77dad0956f77197931d63bb27
Joe Ashworth <joeashworth@gmail.com>
2019-03-18 19:57:31 -0500

    Configure API

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyCQZWrE6so59vqoPTUjdELkAC2ESeAR-rA'
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+})
